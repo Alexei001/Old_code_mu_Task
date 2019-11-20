803e9b859a94beb0628f3a1d1b33e979eabc2dd5
Alexei Vacari <47106150+Alexei001@users.noreply.github.com>
2019-11-20 18:20:42 +0200

    update task

--- Task_Js_old-code-mu/script.js
+++ Task_Js_old-code-mu/script.js
@@ -246,3 +246,413 @@
     let day = prompt('select number of the week', );
     console.log(day)
     console.log(arr[lang][day]); */
+
+//В переменной min лежит число от 0 до 59. 
+//Определите в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
+
+    /* let min = prompt('select the hour', 00);
+
+    switch (min){
+        case (min >= 0 & min <= 15):
+            console.log('min попадает в первую четверть');
+            break;
+        case(min >15 & min <= 30):
+            console.log('min попадает в вторую четверть');
+            break;
+        case(min > 30 & min <= 45):
+            console.log('min попадает в ттретью четверть');
+            break
+        default:
+            console.log('min попадает в четвертую четверть');
+            break;
+    } */
+
+//Переменная lang может принимать 2 значения: 'ru' 'en'. Если она имеет значение 'ru', 
+//то в переменную arr запишем массив дней недели на русском языке, 
+//а если имеет значение 'en' – то на английском. Решите задачу через 2 if, 
+//через switch-case и через многомерный массив без ифов и switch.
+
+    /*let lang = prompt('selected languages!', );
+
+    let arr = [];
+
+     if (lang == "en"){
+        arr = ['monday', 'tuesday', 'Thursday', 'wednesday', 'friday', 'saturday', 'sunday']; 
+    } ;
+    if(lang == 'ru')  {
+        arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресение'];
+    }; */
+
+    /* switch (lang){
+        case 'ru':
+            arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресение'];
+            break;
+        default:
+            arr = ['monday', 'tuesday', 'Thursday', 'wednesday', 'friday', 'saturday', 'sunday'];
+            break;
+    } 
+     console.log(arr);
+    */
+    /* let obj = {
+        'en': ['monday', 'tuesday', 'Thursday', 'wednesday', 'friday', 'saturday', 'sunday'],
+        'ru': ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресение'],
+    }
+
+    console.log(obj[lang]); */
+
+//Если переменная a равна нулю, то выведите 'Верно', иначе выведите 'Неверно'. 
+//Проверьте работу скрипта при a, равном 1, 0, -3.
+
+/*     let num = prompt('select number !', );
+    if(num == 0) {console.log('верно')} else {console.log('неверно')}; */
+
+//Если переменная a равна '1' и по значению и по типу, то выведите 'Верно', 
+//иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном '1', 1, 3.
+
+    /* let num = Number(prompt('write a number!'));
+    if (num == 1 & num === 1){console.log('Correct!!')} else {console.log('Incorrect ;(')}; */
+
+// Если переменная a равна нулю или равна двум, то прибавьте к ней 7, иначе поделите ее на 10. 
+//Выведите новое значение переменной на экран. Проверьте работу скрипта при a, равном 5, 0, -3, 2.
+
+    /* let num = +prompt('write a number!');
+
+    if (num === 0 || num === 2){
+        num += 7;
+        console.log(num);
+    } else { 
+        num /= 10;
+        console.log(num);
+    } */
+
+//Если переменная a равна или меньше 1, а переменная b больше или равна 3, 
+//то выведите сумму этих переменных, иначе выведите их разность (результат вычитания). 
+//Проверьте работу скрипта при a и b, равном 1 и 3, 0 и 6, 3 и 5.
+
+    /* let numA = +prompt('select number a!'), numB = +prompt('write number b!');
+    if (numA <= 1 & numB >= 3 ){
+        console.log(`result: ${numB + numA}`);
+    } else {
+        console.log(` result: ${numB - numA}`);
+    } */
+
+// Переменная num может принимать 4 значения: 1, 2, 3 или 4. Если она имеет значение '1', то в переменную result запишем 'зима', 
+//если имеет значение '2' – 'весна' и так далее. Решите задачу через switch-case.
+
+    /* let num =  +prompt('write a number', 0);
+    switch (num){
+        case 1:
+            console.log('primavara');
+            break;
+        case 2:
+            console.log('vara');
+            break;
+        case 3:
+            console.log('toamna');
+            break;
+        case 4:
+            console.log('iarna');
+            break;
+        default:
+            console.log('not a number 0..4 :(');
+            break;
+    } */
+
+//В переменной day лежит какое-то число из интервала от 1 до 31. 
+//Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
+
+    /* let day = prompt('write a number 1..31!', 0);
+    console.log(day);
+
+        if (day >= 1 & day <= 10){console.log('prima decada')}
+        else if(day > 10 & day <= 20){console.log('a doua decada')}
+        else if (day > 20 & day <= 31){console.log('a treia decada')} 
+        else {console.log('incorrect!')}; */
+
+//В переменной month лежит какое-то число из интервала от 1 до 12. 
+//Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
+
+    /* let month = prompt('select number of month 1..12 ', 0);
+
+    if(month == "12" || month == "1" || month == '2' ){
+        console.log('зима');
+    } else if (month == "3" || month == "4" || month == '5' ){
+        console.log("весна")    
+    } else if (month == "6" || month == "7" || month == '8' ){
+        console.log("лето");
+    } else if (month == "9" || month == "10" || month == '11' ){
+        console.log("осень")
+    } else {
+        console.log('something wrong!')
+    } */
+
+//Дана строка, состоящая из символов, например, 'abcde'. Проверьте, 
+//что первым символом этой строки является буква 'a'. 
+//Если это так - выведите 'да', в противном случае выведите 'нет'.
+
+    /* let str = prompt('write a string!');
+    if (str[0] == 'a'){
+        console.log('yes')
+    } else {
+        console.log('No!')
+    } */
+
+//Дана строка с цифрами, например, '12345'. Проверьте, 
+//что первым символом этой строки является цифра 1, 2 или 3. 
+//Если это так - выведите 'да', в противном случае выведите 'нет'.   
+
+     /* let str = prompt('write a number!', 00000);
+    if (str[0] == '1' || str[0]=='2' || str[0]=='3'){
+        console.log('yes')
+    } else {
+        console.log('No!')
+    } 
+ */
+
+ //Дана строка из 3-х цифр. Найдите сумму этих цифр. 
+ //То есть сложите как числа первый символ строки, второй и третий.
+
+    /* let sstring =prompt('wrie a string-number'), a = +(sstring[0]), b = +(sstring[1]), c = +(sstring[2]);
+    console.log(`number sum:${a+b+c}`); */
+
+//Дана строка из 6-ти цифр. Проверьте, что сумма первых трех цифр равняется сумме вторых трех цифр. 
+//Если это так - выведите 'да', в противном случае выведите 'нет'.
+   
+    /* let sstring ='123123', a=+(sstring[0]),  b = +(sstring[1]), c = +(sstring[2]);
+    let d=+(sstring[3]), e=+(sstring[4]), f=+(sstring[5]), sum1= a+b+c, sum2= d+e+f;
+    if(sum1 == sum2){ console.log('yes')} else {console.log('No')}; */
+
+//Выведите столбец чисел от 1 до 50
+
+     /*let i = 1;
+        while( i <= 50){
+         document.write(i + '<br>');
+         i++;
+    }
+ 
+    for( let i=1; i<=50; i++){
+        document.write(i + '<br>');
+    }*/
+
+//Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for выведите все эти элементы на экран.
+
+    /* let arr =[1, 2, 3, 4, 5];
+
+    for (let i=0; i < arr.length; i++ ){
+        document.write(arr[i] + '<br>');
+    } */
+
+//Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение элементов этого массива.
+
+    /* let result = 1 ;
+    let arr = [2, 3, 4, 5];
+    
+    for (let i=0; i<arr.length; i++ ){
+        result *= arr[i];
+        console.log(result);
+        document.write(result + "<br>");
+    } */
+
+//Дан объект obj с ключами 'Минск', 'Москва', 'Киев' с элементами 'Беларусь', 'Россия', 'Украина'. 
+//С помощью цикла for-in выведите на экран строки такого формата: 'Минск - это Беларусь.'.
+
+    /* let obj={
+        'Минск':"беларусь",
+        "Москва": "Россия",
+        "Киев": "Украина",
+    }
+
+    for (key in obj){
+        console.log(`${key}- is a - ${obj[key]}`);
+    } */
+
+//Выведите столбец чисел от 1 до 100.
+//Выведите столбец чисел от 11 до 33.
+   
+    /*  for (let i=0; i<=100; i++){
+        document.write(i + '<br>');
+    } */
+
+ //Выведите столбец четных чисел в промежутке от 0 до 100.
+
+    /* for(i=0; i<=100; i++){
+        if (i % 2 == 0 ){document.write(i + '<br>')}; 
+    } */
+ 
+ //С помощью цикла найдите сумму чисел от 1 до 100.
+
+    /* let sum = 1;
+    for(let i = 0 ; i <= 100; i++){
+        sum += i
+    }
+    document.write(sum); */
+
+//Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for выведите все эти элементы на экран.
+
+    /* let arr = [1, 2, 3, 4, 5];
+    for (let i = 0 ; i<arr.length;i++){ document.write(arr[i] + ' ')}; */
+
+//Дан массив с элементами [1, 2, 3, 4, 5]. С помощью цикла for найдите сумму элементов этого массива. 
+//Запишите ее в переменную result.
+ 
+    /* let arr = [1, 2, 3, 4, 5], result = 0 ;
+    for (i= 0 ; i< arr.length; i++){
+        result += arr[i];
+        console.log(result);
+    }
+    document.write('result: '+ result ); */
+
+//Дан объект obj. С помощью цикла for-in выведите на экран ключи и элементы этого объекта.
+/* 
+    var obj = {green: 'зеленый', red: 'красный', blue: 'голубой'};
+
+    for(key in obj){
+        document.write(' '+key+':'+obj[key] );
+    } */
+
+//Дан объект obj с ключами Коля, Вася, Петя с элементами '200', '300', '400'. 
+//С помощью цикла for-in выведите на экран строки такого формата: 'Коля - зарплата 200 долларов.'.
+
+    /* let obj = {
+        Коля: '200',
+        Вася: '300',
+        Петя: '400',
+    }
+
+    for(key in obj){
+        document.write(key+ '-money-'+ obj[key]+ ' $ '+'<br>');
+    } */
+
+//Дан массив с элементами 2, 5, 9, 15, 0, 4. 
+//С помощью цикла for и оператора if выведите на экран столбец тех элементов массива, которые больше 3-х, но меньше 10.
+
+    /* let arr = [2, 5, 9, 15, 0, 4];
+    for(i = 0 ; i<arr.length; i++){
+        if(arr[i] > 3 & arr[i] < 10 ){document.write('<h1>' + arr[i] + '<br>')}
+    }; */
+
+//Дан массив с числами. Числа могут быть положительными и отрицательными. 
+//Найдите сумму положительных элементов массива.
+
+   /*  let sum = 0;
+    let arr = [2, -5, 9, -15, 32, 4, -10, -16, 5, 32, -58];
+    for(i = 0 ; i<arr.length; i++){
+        if(arr[i] > 0  ){
+            sum += arr[i];
+            document.write('<h1>' + sum + '<br>')
+        }
+    }
+    document.write('<h1>'+ sum + '<br>'); */
+
+//Дан массив с элементами 1, 2, 5, 9, 4, 13, 4, 10. 
+//С помощью цикла for и оператора if проверьте есть ли в массиве элемент со значением, равным 4. 
+//Если есть - выведите на экран 'Есть!' и выйдите из цикла. Если нет - ничего делать не надо.
+
+    /* let arr = [1, 2, 5, 9, 4, 13, 4, 10];
+        
+        for(let i = 0 ; i<arr.length; i++){
+            if(arr[i] == 4 ){
+                document.write('Есть! значение 4!');
+                break;
+            }
+            console.log(arr[i]);
+        } */
+
+//Дан массив числами, например: [10, 20, 30, 50, 235, 3000]. 
+//Выведите на экран только те числа из массива, которые начинаются на цифру 1, 2 или 5.
+/* 
+    let arr =[10, 20, 30, 50, 235, 3000];
+
+    for(let i =0 ; i<arr.length; i++){
+        string = String(arr[i]);
+        console.log(typeof string);
+        if ( string[0] == '1' || string[0]== '2' || string[0]== '5'){
+            document.write(string + '<br>');
+        }
+
+    } */
+
+//Дан массив с элементами 1, 2, 3, 4, 5, 6, 7, 8, 9. С помощью цикла for создайте строку '-1-2-3-4-5-6-7-8-9-'.
+
+    /* let arr=[1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for(let i = 0; i<arr.length; i++){
+            document.write(`-${arr[i]}`);
+        }
+ */
+
+ //Составьте массив дней недели. С помощью цикла for выведите все дни недели, а выходные дни выведите жирным.
+
+    /* let arr = ['luni', 'marti', 'miercuri', 'joi', 'vineri', 'simbata', 'duminica'];
+     
+        for(let i = 0 ; i<arr.length ; i++){
+            if(i>=0 & i<5 ){
+                document.write(arr[i] + "<br>");
+            } else{
+                document.write("<strong>"+arr[i]+ "<br>");
+            }
+        } */
+
+//Составьте массив дней недели. С помощью цикла for выведите все дни недели, а текущий день выведите курсивом. 
+//Текущий день должен храниться в переменной day.
+
+        /*  let day = 'marti', arr = ['luni', 'marti', 'miercuri', 'joi', 'vineri', 'simbata', 'duminica'];
+                console.log(typeof arr[2]);
+        for(let i = 0 ; i<arr.length; i++){
+          if(arr[i] !== day){
+              document.write(`${arr[i]} <br>`);
+          } else {
+              document.write('<strong>' + arr[i] + '</strong>' + '<br>');
+          }
+        }  */
+    
+//Дано число n=1000. Делите его на 2 столько раз, пока результат деления не станет меньше 50.
+// Какое число получится? Посчитайте количество итераций, необходимых для этого (итерация - это проход цикла), и запишите его в переменную num.
+         /* let num  = 0;
+        for(let n = 1000; n > 50 ; num++ ){
+                n /= 2;
+                console.log(n);
+        }
+        console.log('Nr de iteratiuni' + num);  */
+
+//Даны переменные a = 10 и b = 3. Найдите остаток от деления a на b. Показать решение.
+
+      /*   let a = 10, b = 3 ;
+        result = a % b;
+
+        console.log(result); */
+
+ //Даны переменные a и b. Проверьте, что a делится без остатка на b. Если это так - выведите 'Делится' и результат деления, 
+ //иначе выведите 'Делится с остатком' и остаток от деления. Показать решение.
+
+        /* let a=12, b=35;
+
+        if( a % b == 0){console.log('se imprateste fara rest')} else {document.write('nu se imparteste')}; */
+
+//Возведите 2 в 10 степень. Результат запишите в переменную st.
+
+        /* let st = Math.pow(2, 10);
+        document.write(st); */
+
+//Найдите квадратный корень из 245.
+
+        /* let result = Math.sqrt(245);
+        console.log(result); */
+
+ //Дан массив с элементами 4, 2, 5, 19, 13, 0, 10. 
+ //Найдите квадратный корень из суммы кубов его элементов. Для решения воспользуйтесь циклом for.
+
+        /* let arr = [4, 2, 5, 19, 13, 0, 10], sumCoub, sumSqrt;
+
+        for (let i = 0; i < arr.length; i++){
+           sumCoub =  Math.pow(arr[i], 2);
+           sumSqrt = Math.sqrt(sumCoub);
+            document.write(sumSqrt + ' ');
+        } */
+
+//Найдите квадратный корень из 379. Результат округлите до целых, до десятых, до сотых.
+
+        /* let num = Math.sqrt(379);
+        console.log(num);
+        console.log(Math.round(num));
+        console.log(num.toFixed(2));
+        console.log(num.toFixed(3)); */
